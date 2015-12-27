162430063215abbdda211365d6363594dd732882
Hironobu Nishikokura <nishiko@mindia.jp>
2015-12-27 13:37:13 +0900

    entry_sideがexpenseの時はamountとdue_amountをマイナスにする

--- app/models/kntn.rb
+++ app/models/kntn.rb
@@ -4,6 +4,7 @@ class Kntn
     user = ENV['KINTONE_USER']
     pass = ENV['KINTONE_PASS']
     @app = ENV['KINTONE_APP'].to_i
+    
     @api = Kintone::Api.new(host, user, pass)
   end
 
@@ -11,11 +12,17 @@ class Kntn
     WalletTxn.all.each_with_index do |wt, i|
       record = {}
       WalletTxn.column_names.each do |column_name|
-        next if ['date', 'created_at', 'updated_at'].include?(column_name)
-        record[column_name] = {value: wt.send(column_name)}
+        next if ['created_at', 'updated_at'].include?(column_name)
+        key = column_name.gsub(/_id$/, '_name')
+        if column_name.match(/_id$/)
+          val = wt.send(column_name.gsub(/_id$/, '')).name
+        else
+          val = wt.send(column_name)
+          val = val * (-1) if column_name.match(/amount/) && wt.entry_side == 'expense'
+        end
+        record[key] = {value: val}
       end
       puts "#{i}: saving #{wt.description}"
-      #raise record.inspect
       @api.record.register(@app, record)
     end
   end
@@ -23,8 +30,7 @@ class Kntn
   def remove
     query = 'limit 100' # 500以上にしたらエラーになる(削除が一度に100件しかできないので100にしてある）
     records = @api.records.get(@app, query, [])['records']
-    raise @api.records.get(@app, query, []).inspect
-    is_retry = true if records.present? && records.count < 99
+    is_retry = true if records.present? && records.count >= 100
     ids = records.map{|r| r['$id']['value'].to_i}
     puts 'start to delete'
     @api.records.delete(@app, ids)
